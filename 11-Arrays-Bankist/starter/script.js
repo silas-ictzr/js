69d0799ac10c292a350667af91af0cfc4688600e
Silaictzr <106068367+Silaictzr@users.noreply.github.com>
2023-07-19 18:05:00 +0300

    151

---
+++ 11-Arrays-Bankist/starter/script.js
@@ -0,0 +1,256 @@
+'use strict';
+
+/////////////////////////////////////////////////
+/////////////////////////////////////////////////
+// BANKIST APP
+
+// Data
+const account1 = {
+  owner: 'Jonas Schmedtmann',
+  movements: [200, 450, -400, 3000, -650, -130, 70, 1300],
+  interestRate: 1.2, // %
+  pin: 1111,
+};
+
+const account2 = {
+  owner: 'Jessica Davis',
+  movements: [5000, 3400, -150, -790, -3210, -1000, 8500, -30],
+  interestRate: 1.5,
+  pin: 2222,
+};
+
+const account3 = {
+  owner: 'Steven Thomas Williams',
+  movements: [200, -200, 340, -300, -20, 50, 400, -460],
+  interestRate: 0.7,
+  pin: 3333,
+};
+
+const account4 = {
+  owner: 'Sarah Smith',
+  movements: [430, 1000, 700, 50, 90],
+  interestRate: 1,
+  pin: 4444,
+};
+
+const accounts = [account1, account2, account3, account4];
+
+// Elements
+const labelWelcome = document.querySelector('.welcome');
+const labelDate = document.querySelector('.date');
+const labelBalance = document.querySelector('.balance__value');
+const labelSumIn = document.querySelector('.summary__value--in');
+const labelSumOut = document.querySelector('.summary__value--out');
+const labelSumInterest = document.querySelector('.summary__value--interest');
+const labelTimer = document.querySelector('.timer');
+
+const containerApp = document.querySelector('.app');
+const containerMovements = document.querySelector('.movements');
+
+const btnLogin = document.querySelector('.login__btn');
+const btnTransfer = document.querySelector('.form__btn--transfer');
+const btnLoan = document.querySelector('.form__btn--loan');
+const btnClose = document.querySelector('.form__btn--close');
+const btnSort = document.querySelector('.btn--sort');
+
+const inputLoginUsername = document.querySelector('.login__input--user');
+const inputLoginPin = document.querySelector('.login__input--pin');
+const inputTransferTo = document.querySelector('.form__input--to');
+const inputTransferAmount = document.querySelector('.form__input--amount');
+const inputLoanAmount = document.querySelector('.form__input--loan-amount');
+const inputCloseUsername = document.querySelector('.form__input--user');
+const inputClosePin = document.querySelector('.form__input--pin');
+
+const displayMovements = function (movements) {
+  containerMovements.innerHTML = '';
+
+  movements.forEach(function (mov, i) {
+    const type = mov > 0 ? 'deposit' : 'withdrawal';
+
+    const html = `  <div class="movements__row">
+    <div class="movements__type movements__type--${type}">${i + 1} ${type}</div>
+    <div class="movements__value">${mov}</div>
+  </div>`;
+    containerMovements.insertAdjacentHTML('afterbegin', html);
+  });
+};
+displayMovements(account1.movements);
+
+const createUsernames = function (user) {
+  const username = user
+    .toLowerCase()
+    .split(' ')
+    .map(name => name[0])
+    .join('');
+  return username;
+};
+
+console.log(createUsernames('Steven Thomas Williams'));
+/////////////////////////////////////////////////
+/////////////////////////////////////////////////
+//LECTURE #151
+
+/*
+//LECTURE #150
+const movements = [200, 450, -400, 3000, -650, -130, 70, 1300];
+
+const eurToUsd = 1.1;
+//const movementsUSD = movements.map(function (mov) {
+  //map yeni bir dizi oluşturacağı için bir değişkene atarız
+ // return mov * eurToUsd;
+//});
+/console.log(movements);
+//console.log(movementsUSD);
+
+const movementsUSD = movements.map(mov => mov * eurToUsd);
+console.log(movements);
+console.log(movementsUSD);
+//ikisinde de aynı sonucu alırız
+const movementsUSDfor = [];
+for (const mov of movements) movementsUSDfor.push(mov * eurToUsd);
+console.log(movementsUSDfor);
+
+const movementsDescriptions = movements.map((mov, i, arr) => {
+  `Movement ${i + 1}: You ${mov > 0 ? 'deposited' : 'withdrew'} ${Math.abs(
+    mov
+  )}`;
+  // if (mov > 0) {
+  // return `Movement ${i + 1}: You deposited ${mov}`;
+  //} else {
+  //return `Movement ${i + 1}: You withdrew ${Math.abs(mov)}`;
+  //}
+});
+console.log(movementsDescriptions);
+*/
+
+//LECTURE #149
+
+//MAP METHOD
+/* diziler üzerinde döngü yapmak için kullanabileceğimiz bir metottur. forEach e benzer ancak farklılıkları map diziyi temel alarak yepyeni bir dizi oluşturur orijinal dizi değişmez !.
+Yani map metodu bir dizi alır, bu dizi üzerinde döngüler yapar ve her değişiklikte kodumuzda belittiğimiz bir !!fonksiyon(current * 2) ile mevcut dizi elemanına uygular  */
+
+//FILTER METHOD
+/*orijinal dizideki bir !!koşulu(current > 2) sağlayan ögeleri filtrelemek için kullanılan bir metottur. */
+
+//REDUCE METHOD
+/*orijinal dizinin tüm elemanlarını tek bir değere indirgemek için kullandığımız bir metottur.(dizideki bütün değerleri toplama örneği (acc + current) reduce metodu dizi üzerinde döndükçe döngünün sonunda tüm elemanların toplamını elde edene kadar currenti acc e ekler dolayısıyla gittikçe büyür . Yani sonuçta yeni bir dizi oluşmaz bir değer oluşur)  */
+
+/*
+//LECTURE #148
+//CODING CHALLENGE #1
+const checkDogs = function (dogsJulia, dogsKate) {
+  const dogsJuliaCorrected = dogsJulia.slice();
+  dogsJuliaCorrected.splice(0, 1);
+  dogsJuliaCorrected.splice(-2);
+  //dogsJulia.slice(1, 3); aynı sonucu verir ekleme yöntemini de kullanmak için bunu yazmadık
+  const dogs = dogsJuliaCorrected.concat(dogsKate);
+  console.log(dogs);
+
+  dogs.forEach(function (dog, i) {
+    if (dog >= 3) {
+      console.log(`Dog number ${i + 1} is an adult, and is ${dog} years old`);
+    } else {
+      console.log(`Dog number ${i + 1} 2 is still a puppy`);
+    }
+  });
+};
+//checkDogs([3, 5, 2, 12, 7], [4, 1, 15, 8, 3]);
+checkDogs([9, 16, 6, 8, 3], [10, 5, 6, 1, 4]);
+*/
+/*
+//LECTURE #145
+
+//Map
+const currencies = new Map([
+  ['USD', 'United States dollar'],
+  ['EUR', 'Euro'],
+  ['GBP', 'Pound sterling'],
+]);
+
+currencies.forEach(function (value, key, map) {
+  console.log(`${key}: ${value}`);
+});
+
+//Set
+const currenciesUnique = new Set(['USD', 'GBP', 'USD', 'EUR', 'EUR']);
+console.log(currenciesUnique);
+currenciesUnique.forEach(function (value, _, map) {
+  console.log(`${value}: ${value}`);
+});
+*/
+/*
+//LECTURE #144
+
+const movements = [200, 450, -400, 3000, -650, -130, 70, 1300];
+for (const [i, movement] of movements.entries()) {
+  if (movement > 0) {
+    console.log(`Movement ${i + 1}: You deposited ${movement}`);
+  } else {
+    console.log(`Movement ${i + 1}: You withdrew ${Math.abs(movement)}`);
+  }
+}
+
+console.log('--- FOREACH ---'); //foreach döngüsünden çıkamayız continue ve break buurada çalışmaz
+movements.forEach(function (
+  mov,
+  i,
+  arr //burada sıralama çook önemli ilk parametre her zaman geçerli eleman, ikinci parametre her zaman geçerli index, üçüncü parametre her zaman üzeride döngü yaptığımız dizi olmalı
+) {
+  if (mov > 0) {
+    console.log(`Movement ${i + 1}: You deposited ${mov}`);
+  } else {
+    console.log(`Movement ${i + 1}: You withdrew ${Math.abs(mov)}`);
+  }
+});
+*/
+/*
+//LECTURE #143
+const arr = [23, 11, 64];
+console.log(arr[0]);
+console.log(arr.at(0));
+
+console.log(arr[arr.length - 1]);//son elemanı verir (64)
+console.log(arr.slice(-1)[0]);//arr.slice(-1) sadece bunu yazarsak [64] böyle gelir bu yüzden yanına [0] yazarız dizinin 0. indeksini isteriz (64)
+console.log(arr.at(-1));//son elemanı verir (64)
+
+console.log('jonas'.at(0));
+*/
+/*
+// LECTURE #142
+
+
+
+const movements = [200, 450, -400, 3000, -650, -130, 70, 1300];
+
+/////////////////////////////////////////////////
+
+//SLICE
+let arr = ['a', 'b', 'c', 'd', 'e'];
+console.log(arr.slice(2)); //2. indeks de dahil sona kadar alır
+console.log(arr.slice(2, 4)); //2. indeks dahil 4. index dahil değil
+console.log(arr.slice(-2)); //sondan ikisi
+console.log(arr.slice(-1)); //en sonuncu
+console.log(arr.slice(1, -2)); //1. indeks dahil sondan ikinci dahil değil
+console.log(arr.slice()); //hepsi
+console.log([...arr]); //hepsi
+
+//SPLICE
+//console.log(arr.splice(2));
+arr.splice(-1);
+console.log(arr);
+arr.splice(1, 2);
+console.log(arr);
+
+//REVERSE
+arr = ['a', 'b', 'c', 'd', 'e'];
+const arr2 = ['j', 'i', 'h', 'g', 'f'];
+console.log(arr2.reverse());
+console.log(arr2);
+
+//CONCAT
+const letters = arr.concat(arr2);
+console.log(letters);
+console.log([...arr, ...arr2]);
+
+//JOIN
+console.log(letters.join(' - '));*/
